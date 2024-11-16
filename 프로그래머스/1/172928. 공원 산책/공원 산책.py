7849c25022984e738ebbd952e45bd4efee487603
김진범 <119301068+bum0w0@users.noreply.github.com>
2024-11-16 23:04:28 +0900

    [level 1] Title: 공원 산책, Time: 0.51 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/1/172928. 공원 산책/공원 산책.py
+++ 프로그래머스/1/172928. 공원 산책/공원 산책.py
@@ -3,44 +3,41 @@ def solution(park, routes):
     n, m = len(park), len(park[0])
     
     # 시작 위치 찾기
-    for row in range(n):  # 행
-        for col in range(m):  # 열
-            if park[row][col] == 'S':  # 'S'를 찾으면
-                current_row, current_col = row, col  # 시작 위치 저장 (0, 1)
+    for i in range(n):  # 행 
+        for j in range(m):  # 열 
+            if park[i][j] == 'S':  # 'S'를 찾으면
+                x, y = i, j  # 시작 위치 저장 (0, 1)
                 break
 
     # 방향 정의
     directions = {
-        'N': (-1, 0),  
-        'S': (1, 0),   
-        'E': (0, 1),   
-        'W': (0, -1)   
+        'N': (-1, 0),
+        'S': (1, 0),
+        'E': (0, 1),
+        'W': (0, -1)
     }
- 
+    
     # 경로 처리
     for route in routes:
-        direction, distance = route.split()  # 방향과 거리 분리
-        distance = int(distance)
+        dir, dist = route.split()  # 방향, 거리
+        dist = int(dist)
         
-        # 거리 이동이 완료된 좌표를 저장할 변수
-        temp_row, temp_col = current_row, current_col
+        # 임시 위치 저장
+        temp_x, temp_y = x, y
         
-        # 거리만큼 이동 시도
-        for _ in range(distance):
+        # dist만큼 이동 시도
+        for _ in range(dist):
+
             # 임시 좌표를 저장할 변수 (이동이 가능한지 후에 검사하고 실제 위치를 갱신하지 않을수도 있으므로 사용하는 것)
-            next_row = temp_row + directions[direction][0]
-            next_col = temp_col + directions[direction][1]
+            nx, ny = temp_x + directions[dir][0], temp_y + directions[dir][1]
             
-            # 이동 전 유효성 검사
-            if not (0 <= next_row < n and 0 <= next_col < m) or park[next_row][next_col] == 'X':
-                break  # 조건문이 거짓이면, 이동이 유효하지 않으면 현재 좌표가 갱신되지 않음 (이동 x)
-            temp_row, temp_col = next_row, next_col  # 유효하다면 임시 좌표를 이동이 제대로 완료된 좌표로 간주
+            # 새로운 좌표 계산 전에 유효성 검사
+            if not (0 <= nx < n and 0 <= ny < m) or park[nx][ny] == 'X':
+                break  # # 조건문이 거짓이면, 이동이 유효하지 않으면 현재 좌표가 갱신되지 않음 (이동 x)
+            temp_x, temp_y = nx, ny  # 유효하다면 임시 좌표를 이동이 제대로 완료된 좌표로 간주
             
-        # 중단된 이동을 포함한 좌표가 갱신될 수 있으므로 else를 사용하여 break 없이 반복문이 끝났을 때만 실제 위치를 갱신하도록 함
+        # 중단된 이동을 포함한 좌표가 갱신될 수 있으므로 else를 사용하여 break 없이 반복문이 끝났을 때만 실제 위치를 갱신하도록 함    
         else:
-            current_row, current_col = temp_row, temp_col
-
-    return [current_row, current_col]
-
-# 로봇 강아지가 명령을 수행하는 동안 공원을 넘어가거나 장애물이 있다면 해당 명령을 무시함.
-# 시작위치가 [0,0] 이고 동쪽으로 2칸, 남쪽으로 2칸 이동한다면 [0,0] -> [0,2] -> [2,2]
+            x, y = temp_x, temp_y
+    
+    return [x, y]
