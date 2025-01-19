0b4fb063ac70c64efe46c2ef7956a9a2b12de01d
김진범 <119301068+bum0w0@users.noreply.github.com>
2025-01-19 16:03:09 +0900

    [level 2] Title: [1차] 캐시, Time: 93.59 ms, Memory: 17.3 MB -BaekjoonHub

--- 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.py
+++ 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.py
@@ -1,24 +1,23 @@
-def solution(cacheSize, cities):
+# deque를 사용한 풀이
+from collections import deque
 
-    cache = []
+def solution(cacheSize, cities):
+    
+    cache = deque(maxlen=cacheSize)  # deque에 maxlen 설정
     time = 0
-    # 문제 조건에서 대소문자 구분을 하지 않는다고 함.
-    # "NewYork"이 저장되어 있어도 "newyork"을 찾으려고 하면 캐시미스가 될 수 있기 때문에 도시 이름을 모두 소문자로 처리
-    cities = [city.lower() for city in cities]
 
     for city in cities:
-        if cacheSize == 0:
-            time += 5
-            continue
-
+        city = city.lower()  # 대소문자 구분이 없으므로 소문자로 변환
+        
         if city in cache:
-            cache.pop(cache.index(city))
-            cache.append(city)
+            # 캐시 히트: 기존 위치 제거 후 다시 삽입
+            # 기존에 항목을 인덱스로 찾은 후 pop으로 제거하지 않고 직접 값을 찾아 제거할 수 있는 remove 메소드 사용 (deque 특징)
+            cache.remove(city)
+            cache.append(city) # 가장 최근 사용으로 갱신
             time += 1
         else:
-            if len(cache) == cacheSize:
-                cache.pop(0)
-            cache.append(city)
-            time += 5    
-        
+            # 캐시 미스: 캐시가 꽉 차면 가장 오래된 항목 자동 제거 후 삽입
+            cache.append(city) # deque의 maxlen 덕분에 크기가 초과되면 가장 오래된 항목이 자동으로 제거
+            time += 5
+
     return time
